ca1dd35e52e9a32878afddaed97268567df5fe3e
Griffith <2ndgriff@gmail.com>
2022-07-21 01:58:00 +0300

    chaned set command to write channel preference to a json file, changed quote command to read from said file when choosing server to access

--- commands/quote.js
+++ commands/quote.js
@@ -9,55 +9,49 @@ module.exports = {
         .addStringOption(option => option.setName('person').setDescription('Grabs a quote from the specified person'))
         .addBooleanOption(option => option.setName('recent').setDescription('Grabs most recent quote')),
 	async execute(interaction,client,quoteChannelId) {
-            const channel = client.channels.cache.get(quoteChannelId);
+        const channel = client.channels.cache.get(quoteChannelId);
+        const recent = interaction.options.getBoolean('recent');
+        const person = interaction.options.getString('person');
 
-            if(channel) {
-                const recent = interaction.options.getBoolean('recent');
-                const person = interaction.options.getString('person');
-
-                try {
-                    channel.messages.fetch({ limit: 100 }).then(messages => {
-                        let arraymessages;
-                        if(person !== null) {
-                            arraymessages = messages.filter((el) => el.content.includes(":") 
-                            && el.content.substring(0, el.content.indexOf(":")).toLowerCase().includes(person.toLowerCase()));
-                        } else {
-                            arraymessages = messages.filter((el) => el.content.includes(":"));
-                        }
-                        let quotes = arraymessages.map((el) => el.content)
-
-                        if(quotes.length == 0) {
-                            interaction.reply({content: "No quotes were found with your specified options.", ephemeral: true})
-                        } else {
-                            let chosenQuote;
-                            if(recent) {
-                                chosenQuote = quotes[0]
-                            } else {
-                                chosenQuote = quotes[Math.floor(Math.random() * quotes.length)]
-                            }
-            
-                            let name = chosenQuote.substring(0, chosenQuote.indexOf(":"))
-                            let quote = chosenQuote.substring(chosenQuote.indexOf(":") + 1, chosenQuote.length)
-            
-                            axios.get('https://picsum.photos/250').then((res) => {
-                                let photoUrl = res.request.res.responseUrl
-                                const exampleEmbed = {
-                                    title: `${quote}`,
-                                    description: `*-${name}*`,
-                                    image: {
-                                        url: `${photoUrl}`
-                                    }
-                                };
-                                interaction.reply({embeds: [exampleEmbed]})
-                            })
-                        }
-                    })
-                } catch (err) {
-                    interaction.reply({content: "Error getting quote.", ephemeral: true})
+        try {
+            channel.messages.fetch({ limit: 100 }).then(messages => {
+                let arraymessages;
+                if(person !== null) {
+                    arraymessages = messages.filter((el) => el.content.includes(":") 
+                    && el.content.substring(0, el.content.indexOf(":")).toLowerCase().includes(person.toLowerCase()));
+                } else {
+                    arraymessages = messages.filter((el) => el.content.includes(":"));
                 }
-            } else {
-                interaction.reply({ content: "Quote channel has not been assigned properly for this server.", ephemeral: true})
-            }
+                let quotes = arraymessages.map((el) => el.content)
+
+                if(quotes.length == 0) {
+                    interaction.reply({content: "No quotes were found with your specified options.", ephemeral: true})
+                } else {
+                    let chosenQuote;
+                    if(recent) {
+                        chosenQuote = quotes[0]
+                    } else {
+                        chosenQuote = quotes[Math.floor(Math.random() * quotes.length)]
+                    }
+    
+                    let name = chosenQuote.substring(0, chosenQuote.indexOf(":"))
+                    let quote = chosenQuote.substring(chosenQuote.indexOf(":") + 1, chosenQuote.length)
+    
+                    axios.get('https://picsum.photos/250').then((res) => {
+                        let photoUrl = res.request.res.responseUrl
+                        const exampleEmbed = {
+                            title: `${quote}`,
+                            description: `*-${name}*`,
+                            image: {
+                                url: `${photoUrl}`
+                            }
+                        };
+                        interaction.reply({embeds: [exampleEmbed]})
+                    })
+                }
+            })
+        } catch (err) {
+            interaction.reply({content: "Error getting quote.", ephemeral: true})
+        }
 	},
 };
-
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const fs = require('fs');
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
 const rat = require("./commands/rat");
@@ -9,8 +10,6 @@ const quote = require("./commands/quote")
 const pinned = require("./commands/pinned")
 
 const config = require("./config.json");
-
-let quoteChannelId = "";
 
 client.on('ready', () => {
   console.log('Ready!');
@@ -38,20 +37,55 @@ client.on('interactionCreate', async interaction => {
     await cool.execute(interaction)
   }
 
-  else if(interaction.commandName === "quote") {
-    await quote.execute(interaction,client,quoteChannelId)
-  }
-
   else if(interaction.commandName === "pinned") {
     await pinned.execute(interaction,client)
   } 
+
+  else if(interaction.commandName === "quote") {
+    try {
+      fs.readFile('preferences.json', 'utf-8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+    
+        const serversObj = JSON.parse(data.toString());
+
+        if(serversObj[interaction.guildId]) {
+          quote.execute(interaction,client,serversObj[interaction.guildId])
+        } else {
+          interaction.reply({ content: "Quote channel has not been assigned properly for this server.", ephemeral: true})
+        }
+      });
+    } catch (error) {
+      interaction.reply({ content: "something went wrong with the request", ephemeral: true })
+    }
+  }
   
   else if(interaction.commandName === "set") {
     try {
       quoteChannelId = interaction.channelId;
+
+      fs.readFile('preferences.json', 'utf-8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+    
+        const serversObj = JSON.parse(data.toString());
+
+        serversObj[interaction.guildId] = quoteChannelId
+        const newData = JSON.stringify(serversObj);
+
+        fs.writeFile('preferences.json', newData, (err) => {
+          if (err) {
+              throw err;
+          }
+          console.log("JSON data is saved.");
+        });
+    });
+
       interaction.reply({ content: "quote channel changed successfully!", ephemeral: true})
     } catch(err) {
-      interaction.reply("failure to change quote channel")
+      interaction.reply({ content:"failure to change quote channel", ephemeral: true})
     }
   } 
 });
